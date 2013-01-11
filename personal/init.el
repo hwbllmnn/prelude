2ea0325522daa038cf6596a68bcb0e49d8181270
Andreas Schmitz <schmitz@occamlabs.de>
2013-01-11 14:30:39 +0100

    migrated some settings

--- personal/init.el
+++ personal/init.el
@@ -1,1 +1,16 @@
-(setq prelude-guru nil)
+(transient-mark-mode t)
+
+(setq prelude-guru nil
+      auto-save-interval 0
+      sh-basic-offset 2
+      tab-width 2
+      indent-tabs-mode nil
+      sgml-indent-data t
+      make-backup-files nil)
+
+(global-set-key [(control up)] '(lambda ()
+                                  (interactive)
+                                  (previous-line 6)))
+(global-set-key [(control down)] '(lambda ()
+                                    (interactive)
+                                    (next-line 6)))
